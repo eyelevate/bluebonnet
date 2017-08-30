c30b68b74ba5756a449c0e0b3660d77f02aefc23
woosshie <30732877+woosshie@users.noreply.github.com>
2017-08-29 21:46:09 -0500

    Revert "almost nothing change"
    This reverts commit 84724deded35d95067f3fc103ac27153beff9602.

--- resources/views/home/shop.blade.php
+++ resources/views/home/shop.blade.php
@@ -24,7 +24,6 @@
 	</div>
 </div>
 @endsection
-
 @section('content')
 <div class="container" style="">
 	<h3 class="text-center">OUR COLLECTIONS</h3>
@@ -41,10 +40,10 @@
 						<h5>{{ $collection->name }}</h5>
 						<a class="btn btn-primary" href="#">View Collection</a>	
 					</div>
+					
 				</template>
 			</theme1-shop-card>
 			@endforeach
-			dd($collections);
 		@endif
 	</div>
 </div>
