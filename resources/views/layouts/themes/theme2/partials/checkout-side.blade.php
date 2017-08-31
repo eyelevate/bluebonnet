d2ca5fa59eec4dd1ac81d63ca02568bd824b1765
richard-cmleon <richard@cmleon.com>
2017-08-31 00:08:15 -0500

    Cart and Checkout Pages
    Cart has responsive text sizes styled in -> need to add feature to checkout page as well.

--- resources/views/layouts/themes/theme2/partials/checkout-side.blade.php
+++ resources/views/layouts/themes/theme2/partials/checkout-side.blade.php
@@ -8,14 +8,14 @@
      <div class="col-4">
      <img src="img/themes/theme2/editedRings/goldRing01.jpg">
     </div>
-    <div class="col-7">
+    <div class="col-4">
       <h5>Gold Ring 01</h5>
       {{--  Quantity  --}}
       <small>Quantity: 1</small>
-      <p>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+      <p>Donec id elit non mi porta gravida at eget metus.</p>
     <small>SKU: GR01-200</small>
     </div>
-    <div class="col-1">
+    <div class="col-4">
      <p>$2,000.00</p>
     </div>
 </div>
@@ -26,14 +26,14 @@
      <div class="col-4">
      <img src="img/themes/theme2/editedRings/goldRing01.jpg">
     </div>
-    <div class="col-7">
+    <div class="col-4">
       <h5>Gold Ring 01</h5>
       {{--  Quantity  --}}
       <small>Quantity: 1</small>
-      <p>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+      <p>Donec id elit non mi porta gravida at eget metus.</p>
     <small>SKU: GR01-200</small>
     </div>
-    <div class="col-1">
+    <div class="col-4">
      <p>$2,000.00</p>
     </div>
 </div>
@@ -41,17 +41,17 @@
 <hr>
 
 <div class="row">
-     <div class="col-4 d-flex flex-column">
+     <div class="col-4">
      <img src="img/themes/theme2/editedRings/goldRing01.jpg">
     </div>
-    <div class="col-7 d-flex flex-column">
+    <div class="col-4">
       <h5>Gold Ring 01</h5>
       {{--  Quantity  --}}
       <small>Quantity: 1</small>
-      <p>Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
+      <p>Donec id elit non mi porta gravida at eget metus.</p>
     <small>SKU: GR01-200</small>
     </div>
-    <div class="col-1 d-flex flex-column">
+    <div class="col-4">
      <p>$2,000.00</p>
     </div>
 </div>
@@ -59,91 +59,36 @@
 <hr>
 
 <div class="row">
-     <div class="col-6 d-flex justify-content-start">
-     <small>Subtotal</small>
+     <div class="col-4">
+     &nbsp;<small>Subtotal</small>
     </div>
-    <div class="col-6 d-flex justify-content-end">
-     <small>$6,000.00</small>
+    <div class="col-4">
+    </div>
+    <div class="col-4">
+      <small>$6,000.00</small>
     </div>
 </div>
 
 <div class="row">
-     <div class="col-6 d-flex justify-content-start">
-     <small>Shipping</small>
+     <div class="col-4">
+     &nbsp;<small>Shipping</small>
     </div>
-    <div class="col-6 d-flex justify-content-end">
-     <small>-</small>
+    <div class="col-4">
+    </div>
+    <div class="col-4">
+      <small>-</small>
     </div>
 </div>
 
 <hr>
 
 <div class="row">
-     <div class="col-6 d-flex justify-content-start">
-     Total
+     <div class="col-4">
+      &nbsp;Total
     </div>
-    <div class="col-6 d-flex justify-content-end">
-     $6,000.00
+    <div class="col-4">
+    </div>
+    <div class="col-4">
+      $6,000.00
     </div>
 </div>
-   <!-- <table class="product-table">
-        <caption class="visually-hidden">Shopping cart</caption>
-        <thead>
-          <tr>
-            <th scope="col"><span class="visually-hidden">Product image</span></th>
-            <th scope="col"><span class="visually-hidden">Description</span></th>
-            <th scope="col"><span class="visually-hidden">Quantity</span></th>
-            <th scope="col"><span class="visually-hidden">Price</span></th>
-          </tr>
-        </thead>
-        <tbody data-order-summary-section="line-items">
-          <tr class="product" data-product-id="5904256965" data-variant-id="18916996229" data-product-type="Bracelets">
-            <td class="product__image">
-              <div class="product-thumbnail">
-                <div class="product-thumbnail__wrapper">
-                  <img alt="Attelage Pavé Cuff - Rose Gold & Champagne Diamond" class="product-thumbnail__image" src="//cdn.shopify.com/s/files/1/0039/8002/products/attelage-pave-cuff-rg-cd-front_small.jpg?12345709250930579744" />
-                </div>
-                <span class="product-thumbnail__quantity" aria-hidden="true">1</span>
-              </div>
-
-            </td>
-            <td class="product__description">
-              <span class="product__description__name order-summary__emphasis">Attelage Pavé Cuff - Rose Gold & Champagne Diamond</span>
-              <span class="product__description__variant order-summary__small-text"></span>
-
-            </td>
-            <td class="product__quantity visually-hidden">
-              1
-            </td>
-            <td class="product__price">
-              <span class="order-summary__emphasis">$6,500.00</span>
-            </td>
-          </tr>
-          <tr class="product" data-product-id="395077424" data-variant-id="1063458156" data-product-type="Bracelets">
-            <td class="product__image">
-              <div class="product-thumbnail">
-                <div class="product-thumbnail__wrapper">
-                  <img alt="Bea Hand Harness - Sterling Silver & Black Diamonds" class="product-thumbnail__image" src="//cdn.shopify.com/s/files/1/0039/8002/products/bea_b-18_ss-bd_small.jpg?12345709250930579744" />
-                </div>
-                <span class="product-thumbnail__quantity" aria-hidden="true">1</span>
-              </div>
-
-            </td>
-            <td class="product__description">
-              <span class="product__description__name order-summary__emphasis">Bea Hand Harness - Sterling Silver & Black Diamonds</span>
-              <span class="product__description__variant order-summary__small-text"></span>
-
-            </td>
-            <td class="product__quantity visually-hidden">
-              1
-            </td>
-            <td class="product__price">
-              <span class="order-summary__emphasis">$550.00</span>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-
-      <div class="order-summary__scroll-indicator">
-        Scroll for more items
-      </div> -->
