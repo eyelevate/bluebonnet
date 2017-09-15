fb7ee52eaa92ed566181c9efb52af2cd95755d94
richard-cmleon <richard@cmleon.com>
2017-09-14 23:29:24 -0500

    Modal title

--- resources/views/layouts/themes/theme2/layout.blade.php
+++ resources/views/layouts/themes/theme2/layout.blade.php
@@ -49,7 +49,7 @@
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title">Location</h5>
+                            <h5 class="modal-title">Freya's Fine Jewelry</h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
