10f582f0525942dc9ea7ee35db98541097ddb7e7
richard-cmleon <30804816+richard-cmleon@users.noreply.github.com>
2017-09-22 18:09:33 -0500

    carousel controls

--- resources/views/inventory_items/shop.blade.php
+++ resources/views/inventory_items/shop.blade.php
@@ -32,16 +32,26 @@
 								$idx++;
 							@endphp
 							<div class="item carousel-item text-center" data-slide-number="{{ $idx }}">
-								<video style="width:100%; max-height:350px;" controls>
+								<video style="width:100%; max-height:350px;" autoplay loop muted>
 	    							<source src="{{ asset(str_replace('public/', 'storage/', $video->src)) }}" type="{{ $video->type }}">
 	    						</video>
 							</div>
 							@endforeach
 						@endif
+	
 					</div>
-					<!-- main slider carousel nav controls -->
-
+						{{--  Inventory Carousel Controls  --}}
+							<a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+								<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+								<span class="carousel-control-next-icon" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
 				</div>
+		
+
 			</div>
 			<hr/>
 			<div class="row clearfix">
