ee88482246d3ef3918bcdb5f9b8ddaa7ca9d0628
wondollaballa <onedough83@gmail.com>
2017-09-25 00:16:17 -0700

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,24 +16,19 @@ mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
 
 
-// Core UI Theme
-mix.js('resources/assets/js/themes/coreui/coreui.js', 'public/js/themes/coreui')
-   .js('resources/assets/js/themes/coreui/views/pace.min.js','public/js/themes/coreui')
-   .js('resources/assets/js/themes/coreui/views/main.js', 'public/js/themes/coreui')
-   // .copyDirectory('resources/assets/img/themes/coreui', 'public/img/themes/coreui')
-   // .copyDirectory('resources/assets/fonts/themes/coreui', 'public/fonts/themes/coreui')
-   .sass('resources/assets/sass/themes/coreui/style.scss','public/css/themes/coreui');
+// // Core UI Theme
+// mix.js('resources/assets/js/themes/coreui/coreui.js', 'public/js/themes/coreui')
+//    .js('resources/assets/js/themes/coreui/views/pace.min.js','public/js/themes/coreui')
+//    .js('resources/assets/js/themes/coreui/views/main.js', 'public/js/themes/coreui')
+//    // .copyDirectory('resources/assets/img/themes/coreui', 'public/img/themes/coreui')
+//    // .copyDirectory('resources/assets/fonts/themes/coreui', 'public/fonts/themes/coreui')
+//    .sass('resources/assets/sass/themes/coreui/style.scss','public/css/themes/coreui');
 
-// // Theme 1 - True Gem
-// mix.sass('resources/assets/sass/themes/theme1/theme1.scss','public/css/themes/theme1')
-//    .js('resources/assets/js/themes/theme1/theme1.js','public/js/themes/theme1')
-//    .copyDirectory('resources/assets/img/themes/theme1', 'public/img/themes/theme1');
-
-// Theme 2 - Anna Scheffield
-mix.sass('resources/assets/sass/themes/theme2/theme2.scss','public/css/themes/theme2')
-   .js('resources/assets/js/themes/theme2/theme2.js','public/js/themes/theme2');
-// Ionicons
-mix.sass('resources/assets/fonts/themes/ionicons/scss/ionicons.scss','public/css/themes/ionicons');
+// // Theme 2 - Anna Scheffield
+// mix.sass('resources/assets/sass/themes/theme2/theme2.scss','public/css/themes/theme2')
+//    .js('resources/assets/js/themes/theme2/theme2.js','public/js/themes/theme2');
+// // Ionicons
+// mix.sass('resources/assets/fonts/themes/ionicons/scss/ionicons.scss','public/css/themes/ionicons');
 
 
 // Vue good tables
