102c5ecb760204e08c47bc1ce121e98f3ad8f642
wondollaballa <onedough83@gmail.com>
2017-09-25 00:04:16 -0700

    update

--- webpack.mix.js
+++ webpack.mix.js
@@ -20,14 +20,14 @@ mix.js('resources/assets/js/app.js', 'public/js')
 mix.js('resources/assets/js/themes/coreui/coreui.js', 'public/js/themes/coreui')
    .js('resources/assets/js/themes/coreui/views/pace.min.js','public/js/themes/coreui')
    .js('resources/assets/js/themes/coreui/views/main.js', 'public/js/themes/coreui')
-   .copyDirectory('resources/assets/img/themes/coreui', 'public/img/themes/coreui')
-   .copyDirectory('resources/assets/fonts/themes/coreui', 'public/fonts/themes/coreui')
+   // .copyDirectory('resources/assets/img/themes/coreui', 'public/img/themes/coreui')
+   // .copyDirectory('resources/assets/fonts/themes/coreui', 'public/fonts/themes/coreui')
    .sass('resources/assets/sass/themes/coreui/style.scss','public/css/themes/coreui');
 
-// Theme 1 - True Gem
-mix.sass('resources/assets/sass/themes/theme1/theme1.scss','public/css/themes/theme1')
-   .js('resources/assets/js/themes/theme1/theme1.js','public/js/themes/theme1')
-   .copyDirectory('resources/assets/img/themes/theme1', 'public/img/themes/theme1');
+// // Theme 1 - True Gem
+// mix.sass('resources/assets/sass/themes/theme1/theme1.scss','public/css/themes/theme1')
+//    .js('resources/assets/js/themes/theme1/theme1.js','public/js/themes/theme1')
+//    .copyDirectory('resources/assets/img/themes/theme1', 'public/img/themes/theme1');
 
 // Theme 2 - Anna Scheffield
 mix.sass('resources/assets/sass/themes/theme2/theme2.scss','public/css/themes/theme2')
