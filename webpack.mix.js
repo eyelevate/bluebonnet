b8ee2becf8586dc0e62b908513cc18da87ce53f4
wondollaballa <onedough83@gmail.com>
2017-09-26 08:25:16 -0700

    update fav

--- webpack.mix.js
+++ webpack.mix.js
@@ -102,3 +102,6 @@ mix.js('resources/assets/js/views/sizes/create.js','public/js/views/sizes');
 
 //Video
 // mix.copyDirectory('resources/assets/video','public/video');
+
+//favicon
+mix.copyDirectory('resources/assets/img/favicon','public/img');
