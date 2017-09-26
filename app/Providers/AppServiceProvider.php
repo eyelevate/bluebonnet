9009c900307518deb7b846c9426ee207261ad0e4
wondollaballa <onedough83@gmail.com>
2017-09-25 21:29:04 -0700

    fix ssl and force it in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
+        if (env('APP_ENV') === 'production') {
+            URL::forceSchema('https');
+        }
         // Layouts
         view()->composer([
             'layouts.themes.backend.login',
